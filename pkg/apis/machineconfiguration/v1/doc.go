415c164ee3b069f8c9ef034972e18a4617caffec
Jakub Hrozek <jhrozek@redhat.com>
2019-11-19 23:28:43 +0100

    Forklift the machineconfig API
    Due to a bug in machine-config-operator:
        https://github.com/openshift/machine-config-operator/issues/848
    we can't just import the packages from that project. So instead we're
    going to define the types locally, copied from m-c-o.

---
+++ pkg/apis/machineconfiguration/v1/doc.go
@@ -0,0 +1,4 @@
+// Package v1 contains API Schema definitions for the machineconfiguration v1 API group
+// +k8s:deepcopy-gen=package,register
+// +groupName=machineconfiguration.openshift.io
+package v1
