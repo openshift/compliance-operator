1cb3c021f23db6a195afae0b2f8ac07f3fa94bb0
Juan Antonio Osorio Robles <jaosorior@redhat.com>
2020-04-14 14:06:42 +0300

    Fix compliancecheck result display
    The JSONPath was looking for spec.Result, while it should have been
    spec.result

--- pkg/apis/compliance/v1alpha1/compliancecheck_types.go
+++ pkg/apis/compliance/v1alpha1/compliancecheck_types.go
@@ -35,7 +35,7 @@ type ComplianceCheckSpec struct {
 
 // ComplianceCheck represent a result of a single compliance "test"
 // +kubebuilder:resource:path=compliancechecks,scope=Namespaced
-// +kubebuilder:printcolumn:name="Result",type="string",JSONPath=`.spec.Result`
+// +kubebuilder:printcolumn:name="Result",type="string",JSONPath=`.spec.result`
 type ComplianceCheck struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
