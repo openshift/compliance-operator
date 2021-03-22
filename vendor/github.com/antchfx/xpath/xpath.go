45e687f61b0e18c33275ba8f349cc6dff15c4d02
Juan Antonio Osorio Robles <jaosorior@redhat.com>
2021-03-22 15:24:17 +0200

    Replace xmldom by xmlquery for profile parsing
    xmldom seems to be a stale project, and its current issues present some
    usability limitations for us:
    
    * Excessive resource usage
    * Inability to get full xml content from sub-nodes.
    
    The second point results in us getting malformed descriptions in some
    rules.
    
    This aims to address that by replacing the XML parsing for another
    library that's more active.
    
    This is only used when generated profiles, rules and variables.
    
    Signed-off-by: Juan Antonio Osorio Robles <jaosorior@redhat.com>

--- vendor/github.com/antchfx/xpath/xpath.go
+++ vendor/github.com/antchfx/xpath/xpath.go
@@ -1,5 +1,10 @@
 package xpath
 
+import (
+	"errors"
+	"fmt"
+)
+
 // NodeType represents a type of XPath node.
 type NodeType int
 
@@ -18,6 +23,9 @@ const (
 
 	// CommentNode is a comment node, such as <!-- my comment -->
 	CommentNode
+
+	// allNode is any types of node, used by xpath package only to predicate match.
+	allNode
 )
 
 // NodeNavigator provides cursor model for navigating XML data.
@@ -86,11 +94,68 @@ func (t *NodeIterator) MoveNext() bool {
 }
 
 // Select selects a node set using the specified XPath expression.
+// This method is deprecated, recommend using Expr.Select() method instead.
 func Select(root NodeNavigator, expr string) *NodeIterator {
-	qy, err := build(expr)
+	exp, err := Compile(expr)
 	if err != nil {
 		panic(err)
 	}
-	t := &NodeIterator{query: qy, node: root}
-	return t
+	return exp.Select(root)
+}
+
+// Expr is an XPath expression for query.
+type Expr struct {
+	s string
+	q query
+}
+
+type iteratorFunc func() NodeNavigator
+
+func (f iteratorFunc) Current() NodeNavigator {
+	return f()
+}
+
+// Evaluate returns the result of the expression.
+// The result type of the expression is one of the follow: bool,float64,string,NodeIterator).
+func (expr *Expr) Evaluate(root NodeNavigator) interface{} {
+	val := expr.q.Evaluate(iteratorFunc(func() NodeNavigator { return root }))
+	switch val.(type) {
+	case query:
+		return &NodeIterator{query: expr.q.Clone(), node: root}
+	}
+	return val
+}
+
+// Select selects a node set using the specified XPath expression.
+func (expr *Expr) Select(root NodeNavigator) *NodeIterator {
+	return &NodeIterator{query: expr.q.Clone(), node: root}
+}
+
+// String returns XPath expression string.
+func (expr *Expr) String() string {
+	return expr.s
+}
+
+// Compile compiles an XPath expression string.
+func Compile(expr string) (*Expr, error) {
+	if expr == "" {
+		return nil, errors.New("expr expression is nil")
+	}
+	qy, err := build(expr)
+	if err != nil {
+		return nil, err
+	}
+	if qy == nil {
+		return nil, fmt.Errorf(fmt.Sprintf("undeclared variable in XPath expression: %s", expr))
+	}
+	return &Expr{s: expr, q: qy}, nil
+}
+
+// MustCompile compiles an XPath expression string and ignored error.
+func MustCompile(expr string) *Expr {
+	exp, err := Compile(expr)
+	if err != nil {
+		return &Expr{s: expr, q: nopQuery{}}
+	}
+	return exp
 }
