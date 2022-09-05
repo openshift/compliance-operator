5b271712e493b3e9da93014576e064e3f8a5198f
Juan Antonio Osorio <juan.osoriorobles@eu.equinix.com>
2022-09-05 10:59:54 +0300

    Remove `operator-framework/operator-registry` dependency
    This dependency was not being used and it just bloats the dependencies
    we include in the operator.
    
    Instead, we're using a binary we download from the release site.
    
    Signed-off-by: Juan Antonio Osorio <juan.osoriorobles@eu.equinix.com>

--- tools.go
+++ tools.go
@@ -6,7 +6,6 @@
 package tools
 
 import (
-	_ "github.com/operator-framework/operator-registry/cmd/opm"
 	_ "github.com/securego/gosec/v2/cmd/gosec"
 	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
 )
